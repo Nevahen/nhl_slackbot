16eb27d320f74f4956e9188a1e22c71bdf533612
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2019-05-19 21:24:06 +0300

    bodyparser and look challenge from json body

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import * as Express from 'express';
+import bodyParser = require('body-parser');
 
 const app = Express();
 
 app.listen(8080);
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
-  if (req.query.challenge) {
-    return res.send(req.query.challenge)
+  if (req.body.challenge) {
+    return res.send(req.body.challenge)
   }    
   return next();
 });
